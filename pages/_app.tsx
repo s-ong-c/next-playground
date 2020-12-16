1a0ceb846829c6dd622037c1a5c0b34a6d2ae1c9
s-ong-c <somony9292@gmail.com>
2020-12-16 14:32:03 +0900

    feat: basic directory structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import type { AppProps } from "next/app";
+import '../styles/typography.css';
+
+import type { AppProps } from 'next/app';
+import GlobalStyles from '../styles/GlobalStyles';
+import Head from 'next/head';
+import { Provider } from 'react-redux';
+import React from 'react';
+import store from '../store';
 
 /** 전역 설정  */
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      </Head>
+      <Provider store={store}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
