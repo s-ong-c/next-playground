afea50687ead8a460c0170add949ba0577913d9a
s-ong-c <somony9292@gmail.com>
2020-12-23 22:28:12 +0900

    feat: quill lib setup

---
+++ src/utils/quill/markdownShortcuts/formats/hr.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import Quill from 'quill';
+
+let BlockEmbed = Quill.import('blots/block/embed');
+
+class HorizontalRule extends BlockEmbed {}
+HorizontalRule.blotName = 'hr';
+HorizontalRule.tagName = 'hr';
+
+export default HorizontalRule;
